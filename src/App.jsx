2949260e54fdb35e3392d8ff4a11b20767754593
randy-urriola <randy.urriolah@gmail.com>
2026-08-09 18:15:16 -0500

    Proyecto 1, Catalogo de cursos de guitarra con React + JavaScript/TypeScript

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+
+import Header from './components/Header';
+import Guitar from './components/Guitar';
+import { db } from './data/db';
+
+function App() {
+	const [data, setData] = useState(db);
+	const [cart, setCart] = useState([]);
+
+  // Funcionalidad - Elementos en el carrito
+	function addToCart(item) {
+		const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+
+		if (itemExists >= 0) {
+			const updatedCart = [...cart]; // crear copia del carrito para no modificar el state
+			updatedCart[itemExists].quantity++;
+			setCart(updatedCart);
+		} else {
+			item.quantity = 1;
+			setCart([...cart, item]); // agrega nuevos elementos al carrito
+		}
+	}
+
+	return (
+		<>
+			<Header />
+
+			<main className="container-xl mt-5">
+				<h2 className="text-center">Nuestra Colección</h2>
+
+				<div className="row mt-5">
+					{data.map((guitar) => (
+						<Guitar
+							key={guitar.id}
+							guitar={guitar}
+							setCart={setCart}
+							addToCart={addToCart}
+						/>
+					))}
+				</div>
+			</main>
+
+			<footer className="bg-dark mt-5 py-5">
+				<div className="container-xl">
+					<p className="text-white text-center fs-4 mt-4 m-md-0">
+						GuitarLA - Todos los derechos Reservados
+					</p>
+				</div>
+			</footer>
+		</>
+	);
+}
+
+export default App;
